6ca98409052746db0e6b4d258b7617e817a6a5d1
Tom Haynes <tomhaynes94@gmail.com>
2022-09-19 02:52:48 +0100

    colour flipper completed

--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,20 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+ const btn = document.getElementById('btn');
+ const color = document.querySelector('.color');
+
+ btn.addEventListener('click', () => {
+    let hexValue = '#';
+
+    for(let i = 0; i < 6; i++) {
+        hexValue += hex[getRandomNumber()];
+    }
+
+    color.textContent = hexValue;
+    document.body.style.backgroundColor = hexValue;
+    console.log(hexValue);
+ });
+
+ const getRandomNumber = () => {
+    return Math.floor(Math.random() * hex.length);
+ }
